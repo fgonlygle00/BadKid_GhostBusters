a051d7b056c9c30bea4681d010b9ee890f9b8d77
Eun6636 <tktmdmswl004@gmail.com>
2024-02-09 01:38:30 +0900

    [Add]_2/Monster_controller
    몬스터가 움직일 기반만 세팅

---
+++ Assets/Scripts/Eun_Monster/Monster_Controller.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster_Controller : MonoBehaviour  //이동 회전 로직
+{
+    public Monster_Datas Datas;
+    float moveDistanse;
+
+}
