d137168057e27c8aa620e49ed0c3a1b07a8dd09e
Eun6636 <tktmdmswl004@gmail.com>
2024-02-12 00:08:45 +0900

    [Add]_03/Initialize_Data
    데이터 변수 변경

--- Assets/Scripts/Eun/Monster_Controller.cs
+++ Assets/Scripts/Eun/Monster_Controller.cs
@@ -7,9 +7,7 @@ public class Monster_Controller : MonoBehaviour  //이동 회전 로직
     [SerializeField]private Monster_Status Datas;
     public float moveDistanse;
 
-
-
-    public int Attack()
+    public float Attack()
     {
         return Datas.attack;
     }
