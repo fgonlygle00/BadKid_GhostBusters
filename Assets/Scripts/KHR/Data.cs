8a7908e636e4f42ea4fb5527b528a651a6f47d8d
박지훈 <154571034+fgonlygle00@users.noreply.github.com>
2024-02-14 21:31:04 +0900

    [Fix]_03/GameOver_UI
    - 게임 오버 시 다시 시작하면 다시 시작 안되는 오류 수정

--- Assets/Scripts/KHR/Data.cs
+++ Assets/Scripts/KHR/Data.cs
@@ -9,6 +9,11 @@ public class Data : MonoBehaviour
     public Invasion_Controller invasionController; // Invasion_Controller 스크립트를 불러오기 위한 변수
     public int waveCount = 0; // 현재까지 버틴 웨이브 수
 
+    public void Awake()
+    {
+        Time.timeScale = 1.0f;
+    }
+
     public void Start()
     {
         // Invasion_Controller 스크립트의 인스턴스를 찾아서 할당
@@ -20,11 +25,12 @@ public class Data : MonoBehaviour
         // 게임 오버 체크: 최종 거점의 체력이 0 이하일 경우
         if (invasionController.ReturnHealth() <= 0)
         {
+            Time.timeScale = 0;
             ShowGameOverPanel();
         }
 
         // 게임 클리어 체크: 20 웨이브를 버티면 게임 클리어
-        if (waveCount >= 20)
+        if (Monster_Manager.Instanse.Wave > 20)
         {
             ShowGameClearPanel();
         }
