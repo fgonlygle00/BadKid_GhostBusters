ea6109888abc21cee8c836e445d2a6e9498edf10
Eun6636 <tktmdmswl004@gmail.com>
2024-02-15 15:01:01 +0900

    [Add] BossSound
    보스 사운드 전부 넣음

--- Assets/Scripts/Eun/MonsterSkill/Monster_Bomb.cs
+++ Assets/Scripts/Eun/MonsterSkill/Monster_Bomb.cs
@@ -6,6 +6,8 @@ public class Monster_Bomb : MonoBehaviour
 {
     public int[] bombIndex;
 
+    [SerializeField] private AudioSource _audioSource;
+
     GameObject BombObject;
 
     public GameObject magicCircle;
@@ -14,6 +16,7 @@ public class Monster_Bomb : MonoBehaviour
     GameObject _paticle;
     void Start()
     {
+        _audioSource.clip = AudioManager.instanse._backGroundMusics[6];
         magicCircle.SetActive(false);
     }
 
@@ -37,16 +40,18 @@ public class Monster_Bomb : MonoBehaviour
             BombObject.GetComponent<Density_Enhance_Tower>().Ex_BuffCancel();
         }
 
-        magicCircle.SetActive(true);
+        StartCoroutine(PlayBomb());
         _paticle = Instantiate(bombPaticle);
         _paticle.transform.position = BombObject.transform.position;
-        Invoke("DestroyTowal", 0.5f);
     }
 
    
-
-    void DestroyTowal()
+    IEnumerator PlayBomb()
     {
+        _audioSource.Play();
+        magicCircle.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        _audioSource.Stop();
         magicCircle.SetActive(false);
         Destroy(BombObject);
         Destroy(_paticle);
